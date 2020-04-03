736c2e47cae16241f3462b99489e2fce67fa9751
Speeyush07 <Speeyush.1.S@niit-tech.com>
2020-04-03 16:20:18 +0530

    Solution2

---
+++ Question2/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Question2.Models;
+
+namespace ques_two.Controllers
+{
+    public class employeeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            Employee emp = new Employee();
+
+            var e = emp.get();
+
+            ViewBag.title = "employeelist";
+            ViewBag.emp = e;
+            return View();
+        }
+    }
+}
---
+++ Question2/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ques_two.Models
+{
+    public class Employee
+    {
+
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Employee()
+        {
+
+        }
+
+        public List<Employee> get()
+        {
+
+
+            List<Employee> emp = new List<Employee>()
+        {
+            new Employee{id=1,name="arsh" },
+            new Employee{ id=2,name="abhay"},
+            new Employee{id=3,name="nikhil"},
+            new Employee{id=4,name="piyush"}
+
+        };
+
+            return emp;
+        }
+    }
+}
